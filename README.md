6fc3e628c759ec6ecc0a8688d43b78f66d10a764
zhangfenghua <zhangfenghua.huazi@bytedance.com>
2019-12-17 18:00:34 +0800

    :shit:

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # inline-code-webpack-plugin
-webpack插件，支持将打包结果或者自定义js、css内联插入到html中。依赖htmlwebpackplugin
+webpack插件，支持将打包结果或者自定义js、css内联插入到html中。
+
+```
+entry: {
+		bundle: [''],
+		inlinejs: [''],
+		skeleton: ['']
+},
+plugins: [
+    new htmlWebpackPlugin({
+        template: './index.html',
+        chunks: ['bundle'],
+        inject: false,
+        minify: false
+    }),
+    new InlineCodeWebpackPlugin({
+        // 插入打包后的代码
+        headTags: ['skeleton'], 
+        bodyTags: ['inlinejs', {
+            tagName: 'script',
+            innerHTML: 'var a= 1;', // 注入自定义脚本
+            attributes: {
+                href: '',
+                src: ''
+            }
+        }]
+    }),
+]
+
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+/**
+ * 把打包编译好的脚本插入到html的固定位置
+    {
+        // 插入打包后的代码
+        headTags: ['inlinejs', 'skeleton'], 
+        whateverTags: ['skeleton'],
+        // 直接插入标签
+        contentTags: {
+            tagName: 'script',
+            innerHTML: 'var a= 1;',
+            attributes: {
+                href: '',
+                src: ''
+            }
+        }
+    }
+ */
+module.exports = class InlineCodeWebpackPlugin {
+    constructor(option = {}) {
+        this.headTags = option.headTags || [];
+        this.bodyTags = option.bodyTags || [];
+    } 
+    constructorHTMLTag (compilation) {
+        const entryNames = Object.keys(compilation.assets);
+        const hash = compilation.hash.substr(0,6);
+        const config2tag = (config) => {
+            if (typeof config === 'string') {
+                const assetName = entryNames.filter((t) => t.includes(`${config}-${hash}`))[0];
+                return {
+                    tagName: assetName.match(/.js$/) ? 'script':'style',
+                    innerHTML: compilation.assets[assetName].source()
+                }
+            } else if (typeof config === 'object') {
+                return config;
+            }
+        }
+
+        this.headTags = this.headTags.map(config2tag);
+        this.bodyTags = this.bodyTags.map(config2tag);
+    }
+    apply (compiler) {
+        // HtmlWebpackPlugin version >= 4.0.0-beta.5
+        if (HtmlWebpackPlugin.getHooks) {
+            compiler.hooks.compilation.tap('HtmlWebpackInjectorPlugin', (compilation) => {
+                HtmlWebpackPlugin.getHooks(compilation).alterAssetTagGroups.tapAsync(
+                    'InlineCodeWebpackPlugin', (data, callback) => {
+                        this.constructorHTMLTag(compilation);
+                        data.headTags = this.headTags.concat(data.headTags);
+                        data.bodyTags = this.bodyTags.concat(data.bodyTags);
+                        callback(null, data)
+                    }
+                )
+            });
+        } else {
+            throw 'Please use the latest version of HtmlWebpackPlugin'
+        }
+    }
+}
